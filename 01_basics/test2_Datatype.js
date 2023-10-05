50dffcf4a53aa8aa1536e353e4029f29187bf5d0
gavadesg <31230235+gavadesg@users.noreply.github.com>
2023-10-05 10:45:08 +0000

    datatypes

---
+++ 01_basics/test2_Datatype.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as new JS
+
+let Fname="mahir"
+let age= 32
+
+//number
+//string
+//bigint
+// boolean 
+//null 
+// undefind 
+
+console.log([Fname, age]);
+
+console.log(typeof Fname);
+ 
+console.log(typeof age);
